a28ae911e10369e5bb0ff1f94f60fd75498cab15
WolfRoal <mani_pepe@hotmail.com>
2016-10-26 20:20:00 -0500

    Add files via upload

---
+++ ArbolGjoseManuelrodriguezreglas.pl
@@ -0,0 +1,49 @@
+padrep(josev, jose).
+madrep(rosario,jose).
+esposap(rosario,josev).
+esposop(josev,rosario).
+
+padrem(miguel, anasilvia).
+madrem(rebeca,anasilvia).
+esposam(rebeca,miguel).
+esposom(miguel,rebeca).
+
+esposa(anasilvia,jose).
+esposo(jose,anasilvia).
+
+padre(jose, josem).
+padre(jose, abraham).
+padre(jose, mariana).
+
+madre(anasilvia,jose).
+madre(anasilvia,abraham).
+madre(anasilvia,mariana).
+
+abuelop(josev,mariana).
+abuelop(josev,abraham).
+abuelop(josev,josem).
+
+abuelom(miguel,mariana).
+abuelom(miguel,abraham).
+abuelom(miguel,josem).
+
+abuelam(rebeca,mariana).
+abuelam(rebeca,abraham).
+abuelam(rebeca,josem).
+
+abuelap(rosario,mariana).
+abuelap(rosario,abraham).
+abuelap(rosario,josem).
+
+hermana(mariana,abraham).
+hermana(mariana,josem).
+
+hermano(abraham,josem).
+hermano(abraham,mariana).
+
+hermanom(josem,abraham).
+hermanom(josem,mariana).
+
+abuelo(X,Y):- padre(X,Z), padre(Z,Y).
+hermano(X,Y):- padre(Z,X), padre(Z,Y).
+tio(X,Y):- hermano(X,Z), padre(Z,Y).
